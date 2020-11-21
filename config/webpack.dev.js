5f629e6b6d6d1e1c88a71ac0c0f9f8fa14bf3c8b
mjain <mjain@mediaocean.com>
2020-11-21 15:36:49 +0530

    Added react along with linting and prettier support

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,17 +1,20 @@
 const paths = require('./paths');
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const webpack = require('webpack');
 
+const port = process.env.PORT || 8080;
+
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'source-map',
     devServer: {
         historyApiFallback: true,
         contentBase: paths.build,
         open: true,
         compress: true,
         hot: true,
-        port: 8080
+        port: port
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin()
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import logo from './images/react-lego.jpeg';
-import './styles/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-// Create a class property without a constructor
-class Game {
-    name = 'Violin Charades'
-  }
-  const myGame = new Game();
-
-// Create heading node
-const heading = document.createElement('h1')
-heading.textContent = 'Interesting!'
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-app.append(heading)
-
-app.style.background = 'yellow';
+ReactDOM.render(<App />, document.getElementById('root'));
