d9ee8c2981c9468f3caddb13d5e2adb4beb7e157
mjain <mjain@mediaocean.com>
2020-11-21 16:30:47 +0530

    minor eslintrc changes

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,22 +1,20 @@
-const paths = require('./paths');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
 const webpack = require('webpack');
+const paths = require('./paths');
+const common = require('./webpack.common');
 
 const port = process.env.PORT || 8080;
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-        historyApiFallback: true,
-        contentBase: paths.build,
-        open: true,
-        compress: true,
-        hot: true,
-        port: port
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
-})
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: true,
+    compress: true,
+    hot: true,
+    port,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,40 +1,39 @@
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const paths = require('./paths');
-const {merge} = require('webpack-merge');
 const common = require('./webpack.common');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: false,
-    output: {
-        path: paths.build,
-        publicPath: '/',
-        filename: 'js/[name].[contenthash].bundle.js'
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles/[name].[contenthash].css',
-            chunkFilename: '[id].css'
-        })
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: paths.build,
+    publicPath: '/',
+    filename: 'js/[name].[contenthash].bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2,
-                            sourceMap: false
-                        }
-                    },
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    }
-})
+  },
+});
