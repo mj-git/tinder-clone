88e7b5e86ad0c6cf1751013c8f4cd169c24aeee9
mjain <mjain@mediaocean.com>
2020-11-21 17:51:24 +0530

    Added TS support

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,45 +4,39 @@ const paths = require('./paths');
 
 module.exports = {
   entry: {
-    main: `${paths.src}/index.js`,
+    main: `${paths.src}/index.tsx`
   },
   output: {
     path: paths.build,
     filename: '[name].bundle.js',
-    publicPath: '/',
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Tinder Clone App',
       template: `${paths.src}/template.html`,
       filename: 'index.html',
-      favicon: `${paths.public}/favicon.png`,
+      favicon: `${paths.public}/favicon.png`
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
+        type: 'asset/inline'
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'static/[hash][ext][query]',
-        },
+          filename: 'static/[hash][ext][query]'
+        }
       },
       {
         test: /\.(scss|css)$/,
@@ -53,13 +47,17 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: ['postcss-preset-env'],
-              },
-            },
+                plugins: ['postcss-preset-env']
+              }
+            }
           },
-          'sass-loader',
-        ],
-      },
-    ],
+          'sass-loader'
+        ]
+      }
+    ]
   },
+  resolve: {
+    // Resolve in this order
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.md']
+  }
 };
